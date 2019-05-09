a799e2d96d86fc43f0da7a3b3c57b3fa228d4d99
johnOmena <jvfo@ic.ufal.br>
2019-05-09 00:00:46 -0300

    Adicionando Tarefa01

---
+++ package-test.py
@@ -0,0 +1,24 @@
+import cv2
+import tensorflow
+import keras
+import matplotlib
+import sklearn
+import skimage
+import PIL
+import pandas
+import torch
+
+print("\nBibliotecas instaladas:\n"
+     + "\nOpenCv: " + cv2.__version__ 
+     + "\nTensowflow: " + tensorflow.__version__
+     + "\nKeras: " + keras.__version__
+     + "\nMatplotlib: " + matplotlib.__version__
+     + "\nSklearn: " + sklearn.__version__
+     + "\nSkimage: " + skimage.__version__
+     + "\nPillow: " + PIL.__version__
+     + "\nPandas: " + pandas.__version__
+     + "\nPytorch: " + torch.__version__)
+
+
+
+
