8542b5fc273864e610b91a3af10a28b3dab9571a
John Omena <jvfo@ic.ufal.br>
2019-05-16 15:05:26 -0300

    Aula 2 codigos

--- class-codes/class01.py
+++ class-codes/class01.py
@@ -1,5 +1,5 @@
 #import os
-#folder = '/home/alunoic/PDI-UFAL/screenshots/'
+folder = '/home/alunoic/Downloads'
 #os.path.join(folder, 'imagename.jpg')
 #%% helpers
 help(enumerate)
@@ -8,6 +8,7 @@ help(enumerate)
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 #%% Read a grayscale image
 img = cv2.imread('/home/alunoic/PDI-UFAL/screenshots/cat.jpeg', cv2.IMREAD_GRAYSCALE)
@@ -54,3 +55,52 @@ plt.subplot(121), plt.imshow(img, cmap='gray', vmin=0, vmax=255)
 plt.subplot(122), plt.title('Histogram'), plt.hist(img.ravel(), 256, [0, 256])
 
 plt.show()
+#%% Initialize a random color 
+# O MATPLOTLIB usa o formato RGB enquanto o openCV usa oturo formato, deve-se usar a linha 65 para converter os canais. 
+img = np.ones((15, 15, 3), dtype=np.uint8)
+bgr = cv2.split(img)
+cv2.randu(bgr[0], 0, 255)
+cv2.randu(bgr[1], 0, 255)
+cv2.randu(bgr[2], 0, 255)
+img = cv2.merge(bgr)
+img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.subplot(211), plt.imshow(img)
+plt.subplot(212), plt.imshow(img2)
+#%% Initialize a color image with random values, normaly distributed.
+img = np.ones((250,250,3), np.uint8)
+bgr = cv2.split(img)
+cv2.randn(bgr[0], 127, 40)
+cv2.randn(bgr[1], 127, 40)
+cv2.randn(bgr[2], 127, 40)
+img = cv2.merge(bgr)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.subplot(211), plt.imshow(img)
+plt.subplot(212), plt.title('Histogram'), plt.hist(img.ravel(), 256, [0, 256])
+#%%
+img = np.ones((3, 3), dtype = np.float32)
+cv2.randn(img, 0, 1)
+print(img)
+cv2.normalize(img, img, 255, 0, cv2.NORM_MINMAX)
+print('Normalized = \n', img, '\n\n')
+img = np.asarray(img, dtype = np.uint8)
+print('Converted to uint8 = \n', img, '\n\n')
+# Cast int ( ( (Z - Zmin) / (Zmax - Zmin) ) * 255 ) Como converter de uma faixa de valores para 255 
+#%% Estudar translação em imagens a fundo para a prova.
+# cv2.getRotationMatrix -> Olhar
+# Matriz de rotação e translação para imagens. (Rotacionou e transladou)
+img = cv2.imread(os.path.join(folder, 'lena.png'))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+height, width = img.shape[:2]
+quarter_height, quarter_width = height/4 , width/4
+T = np.float32([
+        [0,-1, quarter_width],
+        [1,0, quarter_height],
+        ])
+img_translation = cv2.warpAffine(
+        img, T, (width, height)
+        )
+plt.subplot(211)
+plt.imshow(img)
+plt.subplot(212)
+plt.imshow(img_translation)
